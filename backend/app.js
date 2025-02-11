81267e8e0ef612f0e5ee3e1a87dfbcaacb4ebf20
RandyFoster1016 <162823487+RandyFoster1016@users.noreply.github.com>
2025-02-11 12:13:31 -0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const port = 8000;
 const client = require('./db/conn.js');
 const cors = require('cors');
 
+app.use(express.static('public'))
 app.use(express.json());
 app.use(cors());
 app.use('/uploads',express.static('uploads'));
